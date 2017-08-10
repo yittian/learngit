2e30515ca70f8b6e1d676bb5900e18d07ffe2847
cyzealy <cyzealy@cyzealydeMacBook-Pro.local>
2017-08-10 11:37:14 +0800

    add coding:utf-8

---
+++ hello.py
@@ -0,0 +1,33 @@
+sample = ['GTA','GGG','CAC']
+
+def read_dna(dna_file):
+	dna_data = ""
+	with open(dna_file,"r") as f:
+		for line in f:
+			dna_data += dna_file
+		return dna_data
+def dna_codons(dna):
+	codons = []
+	for i in range(0,len(dna),3):
+		if (i+3) < len(dna):
+			codons.append(dna[i:i+3])
+	return codons
+def match_dna(dna):
+	matches = 0
+	for condon in dna:
+		if condon in sample:
+			matches += 1
+	return matches
+def is_criminal(dna_sample):
+	dna_data = read_dna(dna_sample)
+	codons = dna_codons(dna_data)
+	num_matches = match_dna(codons)
+	if num_matches >= 3:
+		print "# of codon matches: %s. DNA profile matches. Continue investigation." %num_matches
+	else: 
+		print "Innocent"
+is_criminal('suspect1.txt')
+is_criminal('suspect2.txt')
+is_criminal('suspect3.txt')
+
+
